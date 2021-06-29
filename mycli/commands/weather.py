c105f4bbdf96df01be5c0eadb1fca8808390d8f9
osaidk <osaid.mazin@gmail.com>
2021-06-29 13:53:08 -0500

    some minor changes

--- mycli/commands/weather.py
+++ mycli/commands/weather.py
@@ -1,6 +1,27 @@
 import click
+from mycli.service import weather
+
+class Context:
+    def __init__(self, location):
+        self.location = location
+        self.weather = weather.Weather()
 
 
-@click.command()
-def cli():
-    click.echo("weather file accessed!")
+@click.group()
+@click.option("-l", "--location", type=str, help="Weather at this location.")
+@click.pass_context
+def cli(ctx, location):
+   click.echo('Your weather info for {}: '.format(location))
+   ctx.obj = Context(location)
+
+
+@cli.command()
+@click.pass_context
+def current(ctx):
+    click.echo(ctx.obj.location)
+
+
+@cli.command()
+@click.pass_context
+def forecast(ctx):
+    pass
